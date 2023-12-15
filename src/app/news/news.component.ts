7802b51df7111215bc201d6a5cf2673ca03a3156
D4v1d327 <davide.bruno@edu.itspiemonte.it>
2023-12-15 11:37:09 +0100

    navbar con percorsi coretti

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
@@ -8,22 +7,9 @@ import { Component } from '@angular/core';
 })
 export class NewsComponent {
 
-  isButtonClick = false; 
+  isButtonClick = false;
 
-  wiewMore(){
+  toggleArticle() {
     this.isButtonClick = !this.isButtonClick;
-  }
-
-  wiewSecondNews(){
-    this.isButtonClick = !this.isButtonClick;
-  }
-  
-
-  disableAccordion(){
-    if(this.isButtonClick = true){ 
-
-      this.isButtonClick = false
-        
-    }
   }
 }
