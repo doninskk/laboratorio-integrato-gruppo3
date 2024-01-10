a6fa57cab2ac0fcd12a37207fe4392afc024d776
D4v1d327 <davide.bruno@edu.itspiemonte.it>
2024-01-10 17:52:16 +0100

    aggiustati i JSON per la A1 fem

--- src/app/statistiche/statistiche.component.ts
+++ src/app/statistiche/statistiche.component.ts
@@ -18,37 +18,21 @@ export class StatisticheComponent {
 
   standingA1Femminile: any = [
     {
-      "position": 1,
-
-      "team": {
+        "position": 1,
+        "team": "Conegliano W",
         "id": 704,
-        "name": "Conegliano W",
-        "logo": "https://media.api-sports.io/volley/teams/704.png"
-      },
-      "games": {
-        "played": 15,
+        "logo": "https://media.api-sports.io/volley/teams/704.png",
         "points": 44,
         "form": "WWWWW",
-        "description": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
-      }
+        "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
     },
     {
       "position": 2,
-      "stage": "Serie A1 Women",
-      "group": {
-        "name": "Regular Season"
-      },
-      "team": {
-        "id": 704,
-        "name": "Vero Volley W",
-        "logo": "https://media.api-sports.io/volley/teams/3604.png"
-      },
-      "games": {
-        "played": 15,
-        "points": 40,
-        "form": "WWWWW",
-        "description": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
-      }
+      "team": "Vero Volley W",
+      "id": 704,
+      "logo": "https://media.api-sports.io/volley/teams/3604.png",
+      "form": "WWWWW",
+      "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
     }
   ];
 
