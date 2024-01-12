35c37e970007833ee98480cc4448561e785a66a1
D4v1d327 <davide.bruno@edu.itspiemonte.it>
2024-01-12 16:00:37 +0100

    finita la classifica della A1 Femminile

--- src/app/statistiche/statistiche.component.ts
+++ src/app/statistiche/statistiche.component.ts
@@ -29,12 +29,132 @@ export class StatisticheComponent {
     {
       "position": 2,
       "team": "Vero Volley W",
-      "id": 704,
+      "id": 3604,
       "logo": "https://media.api-sports.io/volley/teams/3604.png",
       "points": 40,
       "form": "WWWWW",
       "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
-    }
+    },
+    {
+      "position": 3,
+      "team": "Scandicci W",
+      "id": 711,
+      "logo": "https://media.api-sports.io/volley/teams/711.png",
+      "points": 38,
+      "form": "WWWWW",
+      "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
+    },
+
+    {
+      "position": 4,
+      "team": "Novara W",
+      "id": 708,
+      "logo": "https://media.api-sports.io/volley/teams/708.png",
+      "points": 33,
+      "form": "WWWLL",
+      "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
+    },
+
+    {
+      "position": 5,
+      "team": "Chieri 76 W",
+      "id": 703,
+      "logo": "https://media.api-sports.io/volley/teams/703.png",
+      "points": 28,
+      "form": "WLWWL",
+      "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
+    },
+
+    {
+      "position": 6,
+      "team": "Pinerolo W",
+      "id": 730,
+      "logo": "https://media.api-sports.io/volley/teams/730.png",
+      "points": 21,
+      "form": "LLWLW",
+      "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
+    },
+
+    {
+      "position": 7,
+      "team": "Vallefoglia W",
+      "id": 714,
+      "logo": "https://media.api-sports.io/volley/teams/714.png",
+      "points": 21,
+      "form": "WWLWL",
+      "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
+    },
+
+    {
+      "position": 8,
+      "team": "Firenze W",
+      "id": 706,
+      "logo": "https://media.api-sports.io/volley/teams/706.png",
+      "points": 19,
+      "form": "WLWLW",
+      "zona": "Promotion - Serie A1 Women (Play Offs: Quarter-finals)"
+    },
+
+    {
+      "position": 9,
+      "team": "Roma W",
+      "id": 710,
+      "logo": "https://media.api-sports.io/volley/teams/710.png",
+      "points": 18,
+      "form": "LLLLW",
+      "zona": "Serie A1 Women (Play Out: )"
+    },
+
+    {
+      "position": 10,
+      "team": "UYBA Busto Arsizio W",
+      "id": 713,
+      "logo": "https://media.api-sports.io/volley/teams/713.png",
+      "points": 14,
+      "form": "LLLWW",
+      "zona": "Serie A1 Women (Play Out: )"
+    },
+
+    {
+      "position": 11,
+      "team": "Cuneo W",
+      "id": 705,
+      "logo": "https://media.api-sports.io/volley/teams/705.png",
+      "points": 13,
+      "form": "LWLLL",
+      "zona": "Serie A1 Women (Play Out: )"
+    },
+
+    {
+      "position": 12,
+      "team": "Bergamo W",
+      "id": 701,
+      "logo": "https://media.api-sports.io/volley/teams/701.png",
+      "points": 11,
+      "form": "LWLLL",
+      "zona": "Serie A1 Women (Play Out: )"
+    },
+
+    {
+      "position": 13,
+      "team": "Casalmaggiore W",
+      "id": 702,
+      "logo": "https://media.api-sports.io/volley/teams/702.png",
+      "points": 9,
+      "form": "LLLLL",
+      "zona": "Relegation - Serie A2 Women"
+    },
+
+    {
+      "position": 14,
+      "team": "Trentino W",
+      "id": 712,
+      "logo": "https://media.api-sports.io/volley/teams/712.png",
+      "points": 3,
+      "form": "LLLLL",
+      "zona": "Serie A1 Women (Play Out: )"
+    },
+
   ];
 
   selectedLeague: string = '';
