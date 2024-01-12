089c3a080e5d6a2bd985c41f6ddb2c75375f849b
doninskk <129967690+doninskk@users.noreply.github.com>
2024-01-12 18:10:02 +0100

    cambio navbar + brutto tentativo di carosello

--- src/app/profilo/profilo.component.ts
+++ src/app/profilo/profilo.component.ts
@@ -11,19 +11,26 @@ class Prize {
     this.description = description;
     this.cost = cost;
     this.progress = progress;
+
+    
   }
 }
+
+
 
 @Component({
   selector: 'app-profilo',
   templateUrl: './profilo.component.html',
   styleUrls: ['./profilo.component.css']
 })
+
+
 export class ProfiloComponent {
   prizes: Prize[] = [
     new Prize("https://www.ikea.com/it/it/images/products/ektorp-divano-letto-a-3-posti-vittaryd-bianco__0734968_PE739647_S5.JPG?f=xs", "Divano", 100, 100),
   ];
   coins: number = 100; // Inizializza le monete a 100
+  
 
   incrementCoins() {
     this.coins += 10;
