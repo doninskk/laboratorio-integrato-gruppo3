c4192ebc2499d6f106e408c2a79d3f7d8981dff9
D4v1d327 <davide.bruno@edu.itspiemonte.it>
2024-01-15 17:09:02 +0100

    funzione al click per le bets non funzionante

--- src/app/gioca/gioca.component.ts
+++ src/app/gioca/gioca.component.ts
@@ -7,78 +7,74 @@ import { partiteServices } from '../servizi/partiteService';
   styleUrls: ['./gioca.component.css']
 })
 export class GiocaComponent {
-
-  bets: any = []
-  users: any = []
-  savedBets : any= []
+  bets: any = [];
+  users: any = [];
+  savedBets: any = [];
   
-  
- betBodyJson = {
-    "week":15,
-    "season":2023,
-    "id_league":97
-};
 
+  betBodyJson = {
+    "week": 15,
+    "season": 2023,
+    "id_league": 97
+  };
 
-
-betBodySave={
-  "token":"",
-  "id_team":751,
-  "id_game":148488
-}
+  betBodySave = {
+    "token": "",
+    "id_team": 751,
+    "id_game": 148488
+  };
 
   ngOnInit() {
     const token = localStorage.getItem('token');
     console.log(token)
 
     if (token) {
-      this.betBodySave = {
-        "token": token,
-        "id_team": 751,
-        "id_game": 148488
-      }}
-    this.betsData()
-    this.betFilterForWeeks()
-    this.userRank()
-    this.savedUserBets()
+      this.betBodySave.token = token;
+    }
+    this.betsData();
+    this.betFilterForWeeks();
+    this.userRank();
+    this.savedUserBets();
   }
 
   constructor(private partiteService: partiteServices) {}
 
-  
-betsData(){
-  this.partiteService.betPageResults().subscribe((response: any) => {
-    this.bets = response;
-    console.log("dati ricevuti",response);
-  },
-  );
+  // Al click sull'icona, chiamata questa funzione con i nuovi valori dell'id_team e id_game
+  setBetBody(teamId: number, gameId: number) {
+    this.betBodySave = {
+      ...this.betBodySave,
+      "id_team": teamId,
+      "id_game": gameId
+    };
+    console.log(this.betBodySave)
+    // Esegui qui eventuali altre operazioni o chiamate di servizio necessarie
+  }
 
-}
-betFilterForWeeks(){
+  betsData() {
+    this.partiteService.betPageResults().subscribe((response: any) => {
+      this.bets = response;
+      console.log("dati ricevuti", response);
+    });
+  }
 
-  this.partiteService.filterForBets(this.betBodyJson).subscribe((response: any) => {
-    this.bets = response;
-    console.log("dati ricevuti",response);
-  },
-  );
-}
+  betFilterForWeeks() {
+    this.partiteService.filterForBets(this.betBodyJson).subscribe((response: any) => {
+      this.bets = response;
+      console.log("dati ricevuti", response);
+    });
+  }
 
+  userRank() {
+    this.partiteService.getUsersPositions().subscribe((response: any) => {
+      this.users = response;
+      console.log("dati ricevuti", response);
+    });
+  }
 
-userRank(){
-  this.partiteService.getUsersPositions().subscribe((response: any) => {
-    this.users = response;
-    console.log("dati ricevuti",response);
-  },
-  ); 
-}
-
-savedUserBets(){
-
-  this.partiteService.getSaveBets(this.betBodySave).subscribe((response: any) => {
-    this.savedBets = response;
-    console.log("dati ricevuti",response);
-  },
-  );
-}
-
+  savedUserBets() {
+    this.partiteService.getSaveBets(this.betBodySave).subscribe((response: any) => {
+      this.savedBets = response;
+      console.log("dati ricevuti", response);
+    });
+  }
 }
