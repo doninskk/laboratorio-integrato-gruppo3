872e569cac87141775295a8990d0c36bff7a9fe2
doninskk <129967690+doninskk@users.noreply.github.com>
2024-01-16 18:02:34 +0100

    commenti ricerca.ts + team-detail.ts

--- src/app/team-detail/team-detail.component.ts
+++ src/app/team-detail/team-detail.component.ts
@@ -1,3 +1,8 @@
+/*
+* Questo componente gestisce la visualizzazione dettagliata di una squadra. 
+* Ottiene l'ID della squadra dalla route e recupera i dettagli della squadra chiamando il servizio partiteService.
+*/
+
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'; 
 import { partiteServices } from '../servizi/partiteService';
@@ -9,8 +14,8 @@ import { partiteServices } from '../servizi/partiteService';
 })
 export class TeamDetailComponent {
 
-  teamDetails: any[] = [];
-  teamId: number | null = null;  // Dichiara una variabile per memorizzare l'ID del team
+  teamDetails: any[] = []; // Array per memorizzare i dettagli della squadra
+  teamId: number | null = null;  // Variabile per memorizzare l'ID della squadra
 
   constructor(
     private partiteService: partiteServices,
@@ -18,11 +23,11 @@ export class TeamDetailComponent {
   ) { }
 
   ngOnInit() {
-    //  l'ID del team dalla route e dall'URL
+    // Ottiene l'ID della squadra dalla route e dall'URL
     const idParam = this.route.snapshot.paramMap.get('id');
     this.teamId = idParam ? +idParam : null;
 
-    // precauzione  si invoca solo se l'id è presente
+    // Verifica se l'ID della squadra è presente e chiama la funzione per ottenere i dettagli della squadra
     if (this.teamId !== null) {
       this.detailOfTeam();
     } else {
@@ -30,10 +35,11 @@ export class TeamDetailComponent {
     }
   }
 
+  // Funzione per ottenere i dettagli della squadra
   detailOfTeam() {
-    // id team nella richiesta
+    // Effettua la chiamata al servizio partiteService per ottenere i dettagli della squadra con l'ID corrente
     this.partiteService.detailTeam({ "id_team": this.teamId }).subscribe((response: any) => {
-      this.teamDetails = response;  // Ora assegno l'array di team
+      this.teamDetails = response;  // Assegna l'array di dettagli della squadra
       console.log("dati ricevuti", response);
     });
   }
