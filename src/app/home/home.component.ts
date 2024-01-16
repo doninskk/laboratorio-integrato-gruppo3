0d90327af6107e329deeb250500ce15103c406f5
doninskk <129967690+doninskk@users.noreply.github.com>
2024-01-16 17:02:21 +0100

    commenti home.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,9 @@
+
+/*
+* Questo componente gestisce la visualizzazione dei risultati delle partite di pallavolo.
+* Include funzionalità di filtraggio per stagione, lega e giornata.
+*/
+
 import { Component } from '@angular/core';
 import { partiteServices } from '../servizi/partiteService';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -8,16 +14,20 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  // Dati di default per la visualizzazione della partita principale (SuperLega)
   risultatiDefaultGame: any={};
+ 
+  // Risultati delle partite da visualizzare
   risultatiPartite: any=[];
-  
+ 
+  // Opzioni per la selezione della stagione
   selectedSeason: string = '';
   seasons = [
     { period: '2023/2024', value: '2023/2024' },
     { period: '2022/2023', value: '2022/2023' },
   ];
  
+  // Opzioni per la selezione della lega
   selectedLeague: string = '';
   leagues = [
     { name: 'SuperLega', value: 'SuperLega',id:97 },
@@ -25,15 +35,17 @@ export class HomeComponent {
     { name: 'A1 Femminile', value: 'A1_Femminile',id:89 }
   ];
   
+  // Settimane disponibili per il filtro
   weeks: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14]; // 10 max SuperLega 14 max  A1 femminile
 
-
+  // Corpo della richiesta per ottenere i dati delle partite
   requestBody = {
     "week": 10,
     "season": 2023,
     "id_league": 97 // 89 id league A1 Femminile
   };
 
+  // ID della partita selezionata
   selectedGameId: number | null = null;
 
   ngOnInit() {
@@ -43,7 +55,7 @@ export class HomeComponent {
   constructor(private partiteService: partiteServices,
               private router: Router) {}
 
-
+  // Recupera i dati della partita di default per la SuperLega
   defaultGameSuperLega(){
     this.partiteService.getDefault().subscribe((response: any) => {
       this.risultatiDefaultGame = response;
@@ -53,6 +65,7 @@ export class HomeComponent {
 
   }
 
+  // Recupera la lista delle partite in base alla richiesta corrente
   gamesList() {
     this.partiteService.getGames(this.requestBody).subscribe((response: any) => {
       this.risultatiPartite = response;
@@ -62,7 +75,7 @@ export class HomeComponent {
 
   }
 
-
+  // Filtra i risultati in base alla settimana selezionata
   weekFilterResults() {
     // Verifica se è stata selezionata una lega
     const selectedLeagueObj = this.leagues.find(league => league.value === this.selectedLeague);
@@ -80,7 +93,7 @@ export class HomeComponent {
   }
   
 
-
+  // Mostra i dettagli della partita selezionata
   showGameDetail(gameId: number) {
     console.log("ID della partita selezionata:", gameId);
     this.selectedGameId = gameId;
