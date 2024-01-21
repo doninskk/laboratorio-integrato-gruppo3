b3f52059c0a05f3cbf237991ae901e7b331698dc
D4v1d327 <davide.bruno@edu.itspiemonte.it>
2024-01-21 17:05:28 +0100

    componente classifica utente impaginata l'API

--- src/app/classifica-utente/classifica-utente.component.ts
+++ src/app/classifica-utente/classifica-utente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PartiteServices } from '../servizi/partiteService';
 
 @Component({
   selector: 'app-classifica-utente',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./classifica-utente.component.css']
 })
 export class ClassificaUtenteComponent {
+  usersStanding: any = []; // array per salvare la classifica degli utenti
+
+  ngOnInit() {
+    // Dati di esempio per la registrazione e il login
+    //this.userSubscribe()
+    //  recupero il token e le informazioni di login dal localStorage all'avvio del componente
+    this.userRank()
+  }
+
+  constructor(private partiteService: PartiteServices) {}
+
+
+  userRank() {
+    this.partiteService.getUsersPositions().subscribe((response: any) => {
+      this.usersStanding = response;
+      console.log("dati ricevuti", response);
+    });
+  }
 
 }
--- src/app/profilo/profilo.component.ts
+++ src/app/profilo/profilo.component.ts
@@ -41,7 +41,6 @@ export class ProfiloComponent implements OnInit {
   private tokenKey: string = 'token'; // Chiave per il token nel localStorage
   private loginInfo: any = {}; // Oggetto per memorizzare le informazioni di login
   userBets: any = [];
-  usersStanding: any = []; // array per salvare la classifica degli utenti
 
   /* fare un form per l'iscrizione e 1 per il login commentato perchè l'utente pippo già esiste
 subscribeBody = {
@@ -85,7 +84,6 @@ subscribeBody = {
     this.betFilterForWeeks();
     this.updateCoins()
     this.allBetsForUsers();
-    this.userRank()
   }
 
   constructor(private partiteService: PartiteServices) {}
@@ -260,13 +258,6 @@ weekFilterResults() {
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
     }
-  }
-
-  userRank() {
-    this.partiteService.getUsersPositions().subscribe((response: any) => {
-      this.usersStanding = response;
-      console.log("dati ricevuti", response);
-    });
   }
   
 }
